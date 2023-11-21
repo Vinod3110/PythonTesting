aad3336704b0bc1f66779df438d3c6d4af528309
Vinod3110 <pawarv40@gmail.com>
2023-11-21 23:08:19 +0530

    Pytest framework learning

---
+++ PythonSelenium/pytestPractice/test_demo1.py
@@ -0,0 +1,7 @@
+#Any pytest file should start with test_ then the name of the file
+#In the file will writting in the function standards
+#method(fuction name should start with test
+
+
+def test_firstProgram():
+    print("Hello first program in the pytest")
